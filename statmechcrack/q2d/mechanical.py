periodic boundaries, now need to fix force calculation (test failing) via helper

statmechcrack/q2d/mechanical.py
@@ -14,8 +14,9 @@ class CrackQ2DMechanical(BasicUtility):
 
     """
     def __init__(
-        self, L=16, N=8*np.ones(9, dtype=int), W=9,
-        kappa=100, alpha=1, varepsilon=100
+        self, L=16, N=8*np.ones(8, dtype=int), W=8,
+        kappa=100, alpha=1, varepsilon=100,
+        periodic_boundary_conditions=False
     ):
         """Initializes the :class:`CrackQ2DMechanical` class.
 
@@ -23,7 +24,6 @@ class CrackQ2DMechanical(BasicUtility):
         from a :class:`BasicUtility` class instance.
 
         """
-        assert(W % 2)
         assert(len(N) == W)
         BasicUtility.__init__(self)
         self.N = N
@@ -34,21 +34,27 @@ class CrackQ2DMechanical(BasicUtility):
         self.alpha = alpha
         self.varepsilon = varepsilon
         self.lambda_TS = 1 + np.log(2)/alpha
-        self.__p_mech_helper = (
-            np.diag(6*np.ones(self.W)) +
-            np.diag(-4*np.ones(self.W - 1), 1) +
-            np.diag(-4*np.ones(self.W - 1), -1) +
-            np.diag(np.ones(self.W - 2), 2) +
-            np.diag(np.ones(self.W - 2), -2)
-        )
-        self.__p_mech_helper[0, 0] = 1
-        self.__p_mech_helper[-1, -1] = 1
-        self.__p_mech_helper[1, 1] = 5
-        self.__p_mech_helper[-2, -2] = 5
-        self.__p_mech_helper[0, 1] = -2
-        self.__p_mech_helper[1, 0] = -2
-        self.__p_mech_helper[-1, -2] = -2
-        self.__p_mech_helper[-2, -1] = -2
+        self.periodic_boundary_conditions = periodic_boundary_conditions
+        if periodic_boundary_conditions is True:
+            self.__p_mech_helper = (
+                np.diag(0.00000000000000000*np.ones(self.W))
+            )
+        else:
+            self.__p_mech_helper = (
+                np.diag(6*np.ones(self.W)) +
+                np.diag(-4*np.ones(self.W - 1), 1) +
+                np.diag(-4*np.ones(self.W - 1), -1) +
+                np.diag(np.ones(self.W - 2), 2) +
+                np.diag(np.ones(self.W - 2), -2)
+            )
+            self.__p_mech_helper[0, 0] = 1
+            self.__p_mech_helper[-1, -1] = 1
+            self.__p_mech_helper[1, 1] = 5
+            self.__p_mech_helper[-2, -2] = 5
+            self.__p_mech_helper[0, 1] = -2
+            self.__p_mech_helper[1, 0] = -2
+            self.__p_mech_helper[-1, -2] = -2
+            self.__p_mech_helper[-2, -1] = -2
         v = symarray('v', self.W)
         s = symarray('s', (self.L, self.W))
         s_vec = Matrix(np.resize(s, self.L*self.W))
@@ -66,9 +72,27 @@ class CrackQ2DMechanical(BasicUtility):
             \right)^2 +
             \sum_{i=0}^{L} \sum_{k=3}^{W} \frac{\kappa}{2} \left(
                 s_i^{k-2} - 2s_i^{k-1} + s_i^k
-            \right)^2
+            \right)^2,
 
         where :math:`s_0^k\equiv v_k`.
+        For periodic boundary conditions,
+
+        .. math::
+            \beta U_0(\mathbf{v}, \mathbf{s}) =
+            \sum_{i=2}^{L} \sum_{k=1}^{W} \frac{\kappa}{2} \left(
+                s_{i-2}^k - 2s_{i-1}^k + s_i^k
+            \right)^2 +
+            \sum_{i=0}^{L} \sum_{k=3}^{W} \frac{\kappa}{2} \left(
+                s_i^{k-2} - 2s_i^{k-1} + s_i^k
+            \right)^2 +
+            \sum_{i=0}^{L} \frac{\kappa}{2} \left[
+                \left(
+                    s_i^{W-1} - 2s_i^W + s_i^1
+                \right)^2 +
+                \left(
+                    s_i^{W} - 2s_i^1 + s_i^2
+                \right)^2
+            \right].
 
         Args:
             v (array_like): The nondimensional end separations.
@@ -79,10 +103,16 @@ class CrackQ2DMechanical(BasicUtility):
 
         """
         vs = np.concatenate(([v], s))
-        return self.kappa/2*(
+        beta_U_0 = self.kappa/2*(
             np.sum(np.diff(vs, 2, axis=0)**2) +
             np.sum(np.diff(vs, 2, axis=1)**2)
         )
+        if self.periodic_boundary_conditions is True:
+            beta_U_0 += self.kappa/2*(
+                np.sum((vs[:, -2] - 2*vs[:, -1] + vs[:, 0])**2) +
+                np.sum((vs[:, -1] - 2*vs[:, 0] + vs[:, 1])**2)
+            )
+        return beta_U_0
 
     def beta_u(self, lambda_):
         r"""The nondimensional potential energy of a single bond

statmechcrack/q2d/tests/test_many.py
@@ -23,185 +23,258 @@ class Many(unittest.TestCase):
 
         """
         self.test_nondimensional_bending_energy_calculation()
-        self.test_nondimensional_mechanical_forces()
         self.test_nondimensional_energy_minimization_no_hessian()
         self.test_nondimensional_energy_minimization_no_hessian_ts()
         self.test_nondimensional_energy_minimization_no_jacobian_hessian()
         self.test_nondimensional_energy_minimization_no_jacobian_hessian_ts()
+        self.test_nondimensional_mechanical_forces()
+        self.test_nondimensional_relative_reaction_rate_coefficients_symmetry()
+
+    def test_nondimensional_relative_reaction_rate_coefficients_symmetry(self):
+        """Function to test that the
+        nondimensional relative reaction rate coefficients are equal
+        for a flat crack front and peridic boundary conditions
+        when the loads are also equal.
+
+        """
+        L = np.random.randint(13, high=18)
+        W = np.random.randint(6, high=10)
+        model = random_crack_model(
+            L=L, W=W,
+            N=np.array(
+                np.random.randint(6, high=9)*np.ones(W),
+                dtype=int
+            ),
+            periodic_boundary_conditions=True
+        )
+        v = np.random.rand()*np.ones(model.W)
+        rate_ref = model.k_isometric(v, 0)
+        for k in range(1, model.W):
+            self.assertAlmostEqual(rate_ref, model.k_isometric(v, k), places=6)
 
     def test_nondimensional_bending_energy_calculation(self):
         """Function to test the nondimensional bending energy calculation.
 
         """
-        model = random_crack_model()
-        v = np.random.rand(model.W)
-        self.assertEqual(v.shape, (model.W,))
-        s = np.random.rand(model.L, model.W)
-        self.assertEqual(s.shape, (model.L, model.W))
-        beta_U_0_model = model.beta_U_0(v, s)
-        vs = np.concatenate(([v], s))
-        self.assertEqual(vs.shape, (model.L + 1, model.W))
-        beta_U_0_check = 0
-        for i in range(2, model.L + 1):
-            for k in range(model.W):
-                beta_U_0_check += model.kappa/2*(
-                    (vs[i - 2, k] - 2*vs[i - 1, k] + vs[i, k])
-                )**2
-        for i in range(model.L + 1):
-            for k in range(2, model.W):
-                beta_U_0_check += model.kappa/2*(
-                    (vs[i, k - 2] - 2*vs[i, k - 1] + vs[i, k])
-                )**2
-        self.assertAlmostEqual(beta_U_0_model, beta_U_0_check)
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
+        )
+        for model in models:
+            v = np.random.rand(model.W)
+            self.assertEqual(v.shape, (model.W,))
+            s = np.random.rand(model.L, model.W)
+            self.assertEqual(s.shape, (model.L, model.W))
+            beta_U_0_model = model.beta_U_0(v, s)
+            vs = np.concatenate(([v], s))
+            self.assertEqual(vs.shape, (model.L + 1, model.W))
+            beta_U_0_check = 0
+            for i in range(2, model.L + 1):
+                for k in range(model.W):
+                    beta_U_0_check += model.kappa/2*(
+                        (vs[i - 2, k] - 2*vs[i - 1, k] + vs[i, k])
+                    )**2
+            for i in range(model.L + 1):
+                for k in range(2, model.W):
+                    beta_U_0_check += model.kappa/2*(
+                        (vs[i, k - 2] - 2*vs[i, k - 1] + vs[i, k])
+                    )**2
+                if model.periodic_boundary_conditions is True:
+                    beta_U_0_check += model.kappa/2*(
+                        (vs[i, model.W - 2] - 2*vs[i, model.W - 1] + vs[i, 0])
+                    )**2 + model.kappa/2*(
+                        (vs[i, model.W - 1] - 2*vs[i, 0] + vs[i, 1])
+                    )**2
+            self.assertAlmostEqual(beta_U_0_model, beta_U_0_check)
 
     def test_nondimensional_mechanical_forces(self):
         """Function to test the nondimensional bending energy calculation.
 
         """
-        model = random_crack_model()
-        v = np.random.rand(model.W)
-        p = model.p_mechanical(v)
-        _, p_same, s, _ = model.minimize_beta_U(v)
-        for k, p_k in enumerate(p):
-            self.assertAlmostEqual(p_k, p_same[k])
-            v_h_p = deepcopy(v)
-            v_h_m = deepcopy(v)
-            v_h_p[k] = v_h_p[k] + h/2
-            v_h_m[k] = v_h_m[k] - h/2
-            p_check_k = (model.beta_U(v_h_p, s) - model.beta_U(v_h_m, s))/h
-            self.assertAlmostEqual(p_k, p_check_k, delta=h)
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
+        )
+        for model in models:
+            v = np.random.rand(model.W)
+            p = model.p_mechanical(v)
+            _, p_same, s, _ = model.minimize_beta_U(v)
+            for k, p_k in enumerate(p):
+                self.assertAlmostEqual(p_k, p_same[k])
+                v_h_p = deepcopy(v)
+                v_h_m = deepcopy(v)
+                v_h_p[k] = v_h_p[k] + h/2
+                v_h_m[k] = v_h_m[k] - h/2
+                p_check_k = (model.beta_U(v_h_p, s) - model.beta_U(v_h_m, s))/h
+                self.assertAlmostEqual(p_k, p_check_k, delta=h)
 
     def test_nondimensional_energy_minimization_no_hessian(self):
         """Function to test the nondimensional energy minimization
         is the same without the hessian.
 
         """
-        model = random_crack_model()
-        v = np.random.rand(model.W)
-        time_0 = time()
-        beta_U, _, s, _ = model.minimize_beta_U(v)
-        time_1 = time()
-        s_vec_guess = np.ones(model.L*model.W)
-        res = minimize(
-            lambda s_vec: model.beta_U(v, s_vec),
-            s_vec_guess,
-            method='Newton-CG',
-            jac=lambda s_vec: model.j_U(v, s_vec)
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
         )
-        beta_U_check = res.fun
-        s_check = np.resize(res.x, (model.L, model.W))
-        time_2 = time()
-        self.assertGreater(time_2 - time_1, time_1 - time_0)
-        self.assertTrue(np.abs((beta_U - beta_U_check)/beta_U_check) < 1e-3)
-        self.assertTrue((np.abs((s - s_check)/s_check) < 5e-2).all())
+        for model in models:
+            v = np.random.rand(model.W)
+            time_0 = time()
+            beta_U, _, s, _ = model.minimize_beta_U(v)
+            time_1 = time()
+            s_vec_guess = np.ones(model.L*model.W)
+            res = minimize(
+                lambda s_vec: model.beta_U(v, s_vec),
+                s_vec_guess,
+                method='Newton-CG',
+                jac=lambda s_vec: model.j_U(v, s_vec)
+            )
+            beta_U_check = res.fun
+            s_check = np.resize(res.x, (model.L, model.W))
+            time_2 = time()
+            self.assertGreater(time_2 - time_1, time_1 - time_0)
+            self.assertTrue(
+                np.abs((beta_U - beta_U_check)/beta_U_check) < 1e-3
+            )
+            self.assertTrue(
+                (np.abs((s - s_check)/s_check) < 5e-2).all()
+            )
 
     def test_nondimensional_energy_minimization_no_hessian_ts(self):
         """Function to test the nondimensional energy minimization
         in a transition state is the same without the hessian.
 
         """
-        model = random_crack_model()
-        v = np.random.rand(model.W)
-        transition_state = np.random.randint(0, high=model.W)
-        time_0 = time()
-        beta_U, _, s, _ = model.minimize_beta_U(
-            v,
-            transition_state=transition_state
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
         )
-        self.assertEqual(
-            s[model.N[transition_state], transition_state],
-            model.lambda_TS
-        )
-        time_1 = time()
-        s_vec_guess = np.ones(model.L*model.W - (transition_state is not None))
-        res = minimize(
-            lambda s_vec: model.beta_U(
-                v, s_vec, transition_state=transition_state
-            ),
-            s_vec_guess,
-            method='Newton-CG',
-            jac=lambda s_vec: model.j_U(
-                v, s_vec, transition_state=transition_state
+        for model in models:
+            v = np.random.rand(model.W)
+            transition_state = np.random.randint(0, high=model.W)
+            time_0 = time()
+            beta_U, _, s, _ = model.minimize_beta_U(
+                v,
+                transition_state=transition_state
             )
-        )
-        beta_U_check = res.fun
-        s_vec_check = res.x
-        s_vec_check = np.insert(
-            s_vec_check,
-            np.ravel_multi_index(
-                (model.N[transition_state], transition_state),
-                (model.L, model.W)
-            ),
-            model.lambda_TS
-        )
-        s_check = np.resize(s_vec_check, (model.L, model.W))
-        time_2 = time()
-        self.assertGreater(time_2 - time_1, time_1 - time_0)
-        self.assertTrue(np.abs((beta_U - beta_U_check)/beta_U_check) < 1e-3)
-        self.assertTrue((np.abs((s - s_check)/s_check) < 5e-2).all())
+            self.assertEqual(
+                s[model.N[transition_state], transition_state],
+                model.lambda_TS
+            )
+            time_1 = time()
+            s_vec_guess = np.ones(
+                model.L*model.W - (transition_state is not None)
+            )
+            res = minimize(
+                lambda s_vec: model.beta_U(
+                    v, s_vec, transition_state=transition_state
+                ),
+                s_vec_guess,
+                method='Newton-CG',
+                jac=lambda s_vec: model.j_U(
+                    v, s_vec, transition_state=transition_state
+                )
+            )
+            beta_U_check = res.fun
+            s_vec_check = res.x
+            s_vec_check = np.insert(
+                s_vec_check,
+                np.ravel_multi_index(
+                    (model.N[transition_state], transition_state),
+                    (model.L, model.W)
+                ),
+                model.lambda_TS
+            )
+            s_check = np.resize(s_vec_check, (model.L, model.W))
+            time_2 = time()
+            self.assertGreater(time_2 - time_1, time_1 - time_0)
+            self.assertTrue(
+                np.abs((beta_U - beta_U_check)/beta_U_check) < 1e-3
+            )
+            self.assertTrue(
+                (np.abs((s - s_check)/s_check) < 5e-2).all()
+            )
 
     def test_nondimensional_energy_minimization_no_jacobian_hessian(self):
         """Function to test the nondimensional energy minimization
         is the same without the jacobian or hessian.
 
         """
-        model = random_crack_model()
-        v = np.random.rand(model.W)
-        time_0 = time()
-        beta_U, _, s, _ = model.minimize_beta_U(v)
-        time_1 = time()
-        s_vec_guess = np.ones(model.L*model.W)
-        res = minimize(
-            lambda s_vec: model.beta_U(v, s_vec),
-            s_vec_guess
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
         )
-        beta_U_check = res.fun
-        s_check = np.resize(res.x, (model.L, model.W))
-        time_2 = time()
-        self.assertGreater(time_2 - time_1, time_1 - time_0)
-        self.assertTrue(np.abs((beta_U - beta_U_check)/beta_U_check) < 1e-3)
-        self.assertTrue((np.abs((s - s_check)/s_check) < 5e-2).all())
+        for model in models:
+            v = np.random.rand(model.W)
+            time_0 = time()
+            beta_U, _, s, _ = model.minimize_beta_U(v)
+            time_1 = time()
+            s_vec_guess = np.ones(model.L*model.W)
+            res = minimize(
+                lambda s_vec: model.beta_U(v, s_vec),
+                s_vec_guess
+            )
+            beta_U_check = res.fun
+            s_check = np.resize(res.x, (model.L, model.W))
+            time_2 = time()
+            self.assertGreater(time_2 - time_1, time_1 - time_0)
+            self.assertTrue(
+                np.abs((beta_U - beta_U_check)/beta_U_check) < 1e-3
+            )
+            self.assertTrue(
+                (np.abs((s - s_check)/s_check) < 5e-2).all()
+            )
 
     def test_nondimensional_energy_minimization_no_jacobian_hessian_ts(self):
         """Function to test the nondimensional energy minimization
         in a transition state is the same without the hessian.
 
         """
-        model = random_crack_model()
-        v = np.random.rand(model.W)
-        transition_state = np.random.randint(0, high=model.W)
-        time_0 = time()
-        beta_U, _, s, _ = model.minimize_beta_U(
-            v,
-            transition_state=transition_state
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
         )
-        self.assertEqual(
-            s[model.N[transition_state], transition_state],
-            model.lambda_TS
-        )
-        time_1 = time()
-        s_vec_guess = np.ones(model.L*model.W - (transition_state is not None))
-        res = minimize(
-            lambda s_vec: model.beta_U(
-                v, s_vec, transition_state=transition_state
-            ),
-            s_vec_guess
-        )
-        beta_U_check = res.fun
-        s_vec_check = res.x
-        s_vec_check = np.insert(
-            s_vec_check,
-            np.ravel_multi_index(
-                (model.N[transition_state], transition_state),
-                (model.L, model.W)
-            ),
-            model.lambda_TS
-        )
-        s_check = np.resize(s_vec_check, (model.L, model.W))
-        time_2 = time()
-        self.assertGreater(time_2 - time_1, time_1 - time_0)
-        self.assertTrue(np.abs((beta_U - beta_U_check)/beta_U_check) < 1e-3)
-        self.assertTrue((np.abs((s - s_check)/s_check) < 5e-2).all())
+        for model in models:
+            v = np.random.rand(model.W)
+            transition_state = np.random.randint(0, high=model.W)
+            time_0 = time()
+            beta_U, _, s, _ = model.minimize_beta_U(
+                v,
+                transition_state=transition_state
+            )
+            self.assertEqual(
+                s[model.N[transition_state], transition_state],
+                model.lambda_TS
+            )
+            time_1 = time()
+            s_vec_guess = np.ones(
+                model.L*model.W - (transition_state is not None)
+            )
+            res = minimize(
+                lambda s_vec: model.beta_U(
+                    v, s_vec, transition_state=transition_state
+                ),
+                s_vec_guess
+            )
+            beta_U_check = res.fun
+            s_vec_check = res.x
+            s_vec_check = np.insert(
+                s_vec_check,
+                np.ravel_multi_index(
+                    (model.N[transition_state], transition_state),
+                    (model.L, model.W)
+                ),
+                model.lambda_TS
+            )
+            s_check = np.resize(s_vec_check, (model.L, model.W))
+            time_2 = time()
+            self.assertGreater(time_2 - time_1, time_1 - time_0)
+            self.assertTrue(
+                np.abs((beta_U - beta_U_check)/beta_U_check) < 1e-3
+            )
+            self.assertTrue(
+                (np.abs((s - s_check)/s_check) < 5e-2).all()
+            )
 
 
 if __name__ == '__main__':

statmechcrack/q2d/tests/test_one.py
@@ -16,15 +16,19 @@ class One(unittest.TestCase):
         """Main function for module-level testing functionality.
 
         """
-        self.test_one_relative_nondimensional_reaction_rate_coefficient()
+        self.test_one_nondimensional_relative_reaction_rate_coefficient()
 
-    def test_one_relative_nondimensional_reaction_rate_coefficient(self):
+    def test_one_nondimensional_relative_reaction_rate_coefficient(self):
         """Function to test for a relative nondimensional
         reaction rate coefficient of one.
 
         """
-        model = random_crack_model()
-        for k in range(model.W):
-            self.assertAlmostEqual(
-                model.k_isometric(np.ones(model.W), k), 1
-            )
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
+        )
+        for model in models:
+            for k in range(model.W):
+                self.assertAlmostEqual(
+                    model.k_isometric(np.ones(model.W), k), 1
+                )

statmechcrack/q2d/tests/test_zero.py
@@ -10,11 +10,12 @@ from ..core import CrackQ2D
 
 def random_crack_model(
     L=np.random.randint(13, high=18),
-    W=1 + 2*np.random.randint(2, high=4),
+    W=np.random.randint(6, high=10),
     N=None,
     kappa=88*(1 - np.random.rand()/2),
     alpha=2*(1 - np.random.rand()/2),
-    varepsilon=88*(1 - np.random.rand()/2)
+    varepsilon=88*(1 - np.random.rand()/2),
+    periodic_boundary_conditions=False
 ):
     """Function to produce a random crack model.
 
@@ -22,7 +23,9 @@ def random_crack_model(
     if N is None:
         N = np.random.randint(6, high=9, size=W)
     return CrackQ2D(
-        L=L, N=N, W=W, kappa=kappa, alpha=alpha, varepsilon=varepsilon
+        L=L, N=N, W=W,
+        kappa=kappa, alpha=alpha, varepsilon=varepsilon,
+        periodic_boundary_conditions=periodic_boundary_conditions
     )
 
 
@@ -34,85 +37,101 @@ class Zero(unittest.TestCase):
         """Main function for module-level testing functionality.
 
         """
+        self.test_zero_minimized_nondimensional_energy()
         self.test_zero_nondimensional_energy()
         self.test_zero_nondimensional_jacobian()
         self.test_zero_nondimensional_end_force()
-        self.test_zero_minimized_nondimensional_energy()
 
     def test_zero_nondimensional_energy(self):
         """Function to test for zero nondimensional energy.
 
         """
         rgn = np.random.rand()
-        model = random_crack_model()
-        self.assertEqual(
-            model.beta_U_0(
-                rgn*np.ones(model.W),
-                rgn*np.ones((model.L, model.W))
-            ), 0
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
         )
-        self.assertEqual(
-            model.beta_U_1(
-                np.ones((model.L, model.W))
-            ), 0
-        )
-        self.assertEqual(
-            model.beta_U(
-                np.ones(model.W),
-                np.ones((model.L*model.W))
-            ), 0
-        )
-        self.assertEqual(
-            model.beta_Pi_0(
-                np.zeros(model.W),
-                rgn*np.ones(model.W),
-                rgn*np.ones((model.L, model.W))
-            ), 0
-        )
-        self.assertEqual(
-            model.beta_Pi(
-                np.zeros(model.W),
-                np.ones(model.W),
-                np.ones((model.L, model.W))
-            ), 0
-        )
+        for model in models:
+            self.assertEqual(
+                model.beta_U_0(
+                    rgn*np.ones(model.W),
+                    rgn*np.ones((model.L, model.W))
+                ), 0
+            )
+            self.assertEqual(
+                model.beta_U_1(
+                    np.ones((model.L, model.W))
+                ), 0
+            )
+            self.assertEqual(
+                model.beta_U(
+                    np.ones(model.W),
+                    np.ones((model.L*model.W))
+                ), 0
+            )
+            self.assertEqual(
+                model.beta_Pi_0(
+                    np.zeros(model.W),
+                    rgn*np.ones(model.W),
+                    rgn*np.ones((model.L, model.W))
+                ), 0
+            )
+            self.assertEqual(
+                model.beta_Pi(
+                    np.zeros(model.W),
+                    np.ones(model.W),
+                    np.ones((model.L, model.W))
+                ), 0
+            )
 
     def test_zero_nondimensional_jacobian(self):
         """Function to test for zero nondimensional Jacobian.
 
         """
         rgn = np.random.rand()
-        model = random_crack_model()
-        v = rgn*np.ones(model.W)
-        s_vec = rgn*np.ones(model.L*model.W)
-        j_U_0 = model.j_U_0(v, s_vec)
-        for j_U_0_ik in j_U_0:
-            self.assertAlmostEqual(j_U_0_ik, 0)
-        s = np.random.rand(model.L, model.W)
-        for j in range(model.W):
-            s[-model.M[j]:, j] = 1
-        j_U_1 = model.j_U_1(np.reshape(s, (model.L, model.W)))
-        for j_U_1_ik in j_U_1:
-            self.assertAlmostEqual(j_U_1_ik, 0)
-        v = np.ones(model.W)
-        s_vec = np.ones(model.L*model.W)
-        j_U = model.j_U(v, s_vec)
-        for j_U_ik in j_U:
-            self.assertAlmostEqual(j_U_ik, 0)
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
+        )
+        for model in models:
+            v = rgn*np.ones(model.W)
+            s_vec = rgn*np.ones(model.L*model.W)
+            j_U_0 = model.j_U_0(v, s_vec)
+            for j_U_0_ik in j_U_0:
+                self.assertAlmostEqual(j_U_0_ik, 0)
+            s = np.random.rand(model.L, model.W)
+            for j in range(model.W):
+                s[-model.M[j]:, j] = 1
+            j_U_1 = model.j_U_1(np.reshape(s, (model.L, model.W)))
+            for j_U_1_ik in j_U_1:
+                self.assertAlmostEqual(j_U_1_ik, 0)
+            v = np.ones(model.W)
+            s_vec = np.ones(model.L*model.W)
+            j_U = model.j_U(v, s_vec)
+            for j_U_ik in j_U:
+                self.assertAlmostEqual(j_U_ik, 0)
 
     def test_zero_minimized_nondimensional_energy(self):
         """Function to test for zero minimized nondimensional energy.
 
         """
-        model = random_crack_model()
-        self.assertAlmostEqual(
-            model.minimize_beta_U(np.ones(model.W))[0], 0
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
         )
+        for model in models:
+            self.assertAlmostEqual(
+                model.minimize_beta_U(np.ones(model.W))[0], 0
+            )
 
     def test_zero_nondimensional_end_force(self):
         """Function to test for zero nondimensional end force.
 
         """
-        model = random_crack_model()
-        for p_k in model.p_mechanical(np.ones(model.W)):
-            self.assertAlmostEqual(p_k, 0)
+        models = (
+            random_crack_model(),
+            random_crack_model(periodic_boundary_conditions=True)
+        )
+        for model in models:
+            for p_k in model.p_mechanical(np.ones(model.W)):
+                self.assertAlmostEqual(p_k, 0)